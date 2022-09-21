452f0b2048d5adc18fc715da1b74250b989f28af
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2022-09-21 11:55:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 let submitButton = document.querySelector(".ending");
-let displayPage =  document.querySelector(".next-page");
+let displayPage = document.querySelector(".next-page");
 let select = document.querySelectorAll(".circle");
-let rating =  document.querySelector(".protect");
-let firstPage = document.querySelector(".first-page")
+let rating = document.querySelector(".protect");
+let firstPage = document.querySelector(".first-page");
 
+submitButton.addEventListener("click", function () {
+  document.querySelector(".next-page").classList.remove("hidden");
+  firstPage.classList.add("hidden");
 
-submitButton.addEventListener("click",function(){
-    document.querySelector(".next-page").classList.remove("hidden");
-    firstPage.classList.add("hidden");
-
-    select.forEach(function(rate){
-        rate.addEventListener("click", function(){
-            rating.innerHTML = rate.innerHTML;
-        })
-        })
-    
-
-})
-
-
-
-
-
+  for (const each of select) {
+    if (each.clicked == true) {
+      rating.textContent = "You selected " + checkedInput.getAttribute("value") + " out of 5";
+    }
+  }
+});
